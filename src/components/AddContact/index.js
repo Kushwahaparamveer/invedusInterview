c71ea4f722fabbe39c9391c7be74416a0369f8de
Paramveer kushwaha <kushwahaparamveer@gmail.com>
2022-02-25 21:11:46 +0530

    first commit

---
+++ src/components/AddContact/index.js
@@ -0,0 +1,123 @@
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { useHistory } from "react-router";
+import { toast } from "react-toastify";
+
+const AddPost = ({ contacts, addContact }) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [type, setType] = useState("");
+  const [checked, setChecked] = useState(false);
+  const history = useHistory();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const checkContactEmailExists = contacts.filter((contact) =>
+      contact.email === email ? contact : null
+    );
+    const checkContactPhoneExists = contacts.filter((contact) =>
+      contact.phone === phone ? contact : null
+    );
+
+    if (!email || !name || !phone) {
+      return toast.warning("Please fill in all fields!!");
+    }
+    if (checkContactEmailExists.length > 0) {
+      return toast.error("This email already exists!!");
+    }
+    if (checkContactPhoneExists.length > 0) {
+      return toast.error("This phone number already exists!!");
+    }
+let dataArr= []
+    const data = {
+      id: contacts.length > 0 ? contacts[contacts.length - 1].id + 1 : 0,
+      email,
+      name,
+      phone,
+      type,
+      checked,
+    };
+dataArr.push(data);
+console.log("wala" ,dataArr)
+localStorage.setItem("data", JSON.stringify(dataArr));
+// localStorage.setItem('data', dataArr);
+    addContact(data);
+    toast.success("Contact added successfully!!");
+    history.push("/");
+  };
+
+  return (
+    <div className="container-fluid">
+      <h1 className="text-center text-dark py-3 display-2">Add Post</h1>
+      <div className="row">
+        <div className="col-md-6 p-5 mx-auto shadow">
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <input
+                className="form-control"
+                type="text"
+                placeholder="Full name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+            <div className="form-group">
+              <input
+                className="form-control"
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="form-group">
+              <input
+                className="form-control"
+                type="number"
+                placeholder="Phone"
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+              />
+            </div>
+            <div className="form-control">
+              <select value={type} onChange={(e) => setType(e.target.value)}>
+              <option value="">Choose Type</option>
+                <option value="office">Office</option>
+                <option value="personal">Personal</option>
+              </select>
+            </div><br />
+            <div className="form-group form-inline">
+            <label>
+            <input
+             type="checkbox"
+             checked={checked}
+             onChange={(e) => setChecked(e.target.checked)}
+             />{" "}
+             &nbsp;&nbsp;This is my whatApp number
+           </label>
+           </div>
+            <div className="form-group">
+              <input
+                className="btn btn-block btn-dark"
+                type="submit"
+                value="Add Contact"
+              />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const mapStateToProps = (state) => ({
+  contacts: state,
+});
+const mapDispatchToProps = (dispatch) => ({
+  addContact: (data) => {
+    dispatch({ type: "ADD_CONTACT", payload: data });
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(AddPost);
